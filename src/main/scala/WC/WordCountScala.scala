8f14e3637566e7ec635c0935f1de9d7f3b874300
CharlieFeng <fsy19920220@gmail.com>
2017-11-19 23:22:46 +1100

    maven add spark, scala, groovy dependencies

---
+++ src/main/scala/WC/WordCountScala.scala
@@ -0,0 +1,24 @@
+package WC
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by charlie on 19/11/17.
+  */
+object WordCountScala {
+  def main(args : Array[String])  {
+    val conf = new SparkConf()
+                  .setMaster("local")
+                  .setAppName("WordCountJava")
+
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("src/resources/words")
+
+    data.flatMap(_.split(","))
+          .map((_,1))
+            .reduceByKey(_+_)
+              .collect()
+                .foreach(println)
+  }
+}
---
+++ src/test/groovy/WCTest/WordCountTest.groovy
@@ -0,0 +1,23 @@
+package WCTest
+
+import WC.WordCountJava
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Created by charlie on 19/11/17.
+ */
+class WordCountTest extends Specification {
+
+    WordCountJava wc
+
+    def setup(){
+        wc = new WordCountJava()
+    }
+
+    @Unroll
+    def "load word count file"(){
+
+        wc.run("src/resources/words")
+    }
+}
