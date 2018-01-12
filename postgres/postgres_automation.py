faf7627528543c8f100a506d3028bfb632415ad3
charliefeng <siyu@airwallex.com>
2018-01-12 11:59:33 +1100

    finish postgres image automation

--- postgres/postgres_automation.py
+++ postgres/postgres_automation.py
@@ -4,37 +4,70 @@ import os
 import shutil
 import docker.errors
 
-tag = 'test'
+DEV_REPOSITORY = 'registry.airwallex.com/postgres'
+PRO_REPOSITORY = ''
+TAG = '1712'
+
+BASE_IMAGE = 'postgres_base:' + TAG
+BASE_CONTAINER = 'postgres_base'
+
+PERSIST_IMAGE = 'postgres_persist:' + TAG
+PERSIST_CONTAINER = 'postgres_persist_' + TAG
+
+DEV_IMAGE = DEV_REPOSITORY + ':' + TAG
+PRO_IMAGE = PRO_REPOSITORY + ':' + TAG
+
 
 client = docker.from_env()
 
 try:
-  prev_base_image = client.images.get('postgres_base:test')
-  client.images.remove('postgres_base:test', force=True)
+  prev_base_image = client.images.get(BASE_IMAGE)
+  client.images.remove(BASE_IMAGE, force=True)
 except docker.errors.ImageNotFound:
   pass
 
-base_image = client.images.build(path='.', tag='postgres_base:test', dockerfile='DockerfileBase')
+base_image = client.images.build(path='.', tag=BASE_IMAGE, dockerfile='DockerfileBase')
 
 try:
-  prev_base_container = client.containers.get('postgres_base')
+  prev_base_container = client.containers.get(BASE_CONTAINER)
   prev_base_container.remove(force = True)
 except docker.errors.NotFound:
   pass
 
-base_container = client.containers.run(image='postgres_base:test', name='postgres_base', ports={'5432/tcp':5432}, detach=True)
+base_container = client.containers.run(image=BASE_IMAGE, name=BASE_CONTAINER, ports={'5432/tcp':5432}, detach=True)
 
-time.sleep(100)
+time.sleep(120)
 #db_data, stat = postgres_base.get_archive('/var/lib/postgresql/data') // this method need to improve
 
 if os.path.exists('dbdata'): shutil.rmtree('dbdata')
 os.system('docker cp postgres_base:/var/lib/postgresql/data dbdata')
 
-persist_image = client.images.build(path='.', tag='charliefeng/postgres_persist:test', dockerfile='DockerfilePersist')
+try:
+  prev_persist_image = client.images.get(PERSIST_IMAGE)
+  client.images.remove(PERSIST_IMAGE, force=True)
+except docker.errors.ImageNotFound:
+  pass
+
+persist_image = client.images.build(path='.', tag=PERSIST_IMAGE, dockerfile='DockerfilePersist')
 
 base_container.stop()
-client.images.remove('postgres_base:test', force=True)
+base_container.remove(force = True)
+client.images.remove(BASE_IMAGE, force=True)
 if os.path.exists('dbdata'): shutil.rmtree('dbdata')
 
-client.images.push(repository= 'charliefeng/postgres_persist:' + tag, auth_config={'username':'charliefeng', 'password':'lyf61825'})
+try:
+    prev_dev_image = client.images.get(DEV_IMAGE)
+    client.images.remove(DEV_IMAGE, force=True)
+except docker.errors.ImageNotFound:
+    pass
+os.system('docker tag ' + PERSIST_IMAGE + ' ' + DEV_IMAGE)
+
+# try:
+#     prev_pro_image = client.images.get(PRO_IMAGE)
+#     client.images.remove(PRO_IMAGE, force=True)
+# except docker.errors.ImageNotFound:
+#     pass
+#os.system('docker tag ' + PERSIST_IMAGE + ' ' + PRO_IMAGE)
+
+client.images.push(repository= DEV_IMAGE)
 
