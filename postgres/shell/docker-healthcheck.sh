963c8b094f01d8f8f5ac5f0d7f189a5afe76fad6
charliefeng <siyu@airwallex.com>
2018-01-18 15:36:30 +1100

    add health check

---
+++ postgres/shell/docker-healthcheck.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+host="$(hostname -i || echo '127.0.0.1')"
+user="${POSTGRES_USER:-postgres}"
+db="${POSTGRES_DB:-validation}"
+export PGPASSWORD="${POSTGRES_PASSWORD:-}"
+
+args=(
+	# force postgres to not use the local unix socket (test "external" connectibility)
+	--host "$host"
+	--username "$user"
+	--dbname "$db"
+	--quiet --no-align --tuples-only
+)
+
+if select="$(echo 'SELECT 1' | psql "${args[@]}")" && [ "$select" = '1' ]; then
+	exit 0
+fi
+
+exit 1
